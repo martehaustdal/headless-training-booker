98a17e50129bf109455bb507f07bb41feff05e0a
martehaustdal <marteh.austdal@gmail.com>
2018-03-11 13:09:32 +0100

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -5,10 +5,10 @@ const mobility = '#item-8 > button';
 const tuesdayBootCamp = '#item-10 > button';
 const thursdayBootCamp = '#item-8 > button';
 
-const tuesdayBootCampBooker = schedule.scheduleJob({hour: 21, minute: 1, second: 2, dayOfWeek: 5}, function(){
+const tuesdayBootCampBooker = schedule.scheduleJob({hour: 21, minute: 0, second: 29, dayOfWeek: 5}, function(){
     book.bookTrainingClass(tuesdayBootCamp, "Bootcamp tirsdag");
 });
-const thursdayBootCampBooker = schedule.scheduleJob({hour: 21, minute: 1, second: 2, dayOfWeek: 5}, function(){
+const thursdayBootCampBooker = schedule.scheduleJob({hour: 21, minute: 0, second: 31, dayOfWeek: 0}, function(){
     book.bookTrainingClass(thursdayBootCamp, "Bootcamp torsdag");
 });
 const tuesdayMobilityBooker = schedule.scheduleJob({hour: 21, minute: 1, second: 2, dayOfWeek: 5}, function(){
